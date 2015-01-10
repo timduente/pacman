3198e58d6dd49e5d5495d3f42fb9ab8105a927cc
timduente <tim.duente@t-online.de>
2015-01-10 13:27:20 +0100

    Änderungen am Auswahlprozess für Aktionen aus dem matchingSet. Aktionen werden nun nur noch ausgewählt, wenn sie besser als die durchschnittliche Prediction des MatchingSets sind.

--- PacMan/src/game/entries/pacman/group4/ActionChooser.java
+++ PacMan/src/game/entries/pacman/group4/ActionChooser.java
@@ -12,90 +12,37 @@ public class ActionChooser implements IActionChooser {
 			ArrayList<IStarCSObject> actionSet,
 			ArrayList<IStarCSObject> matchingSetMinusActionSet) {
 
-//		ArrayList<ArrayList<IStarCSObject>> possibleActionSets = new ArrayList<ArrayList<IStarCSObject>>(
-//				EnvironmentObserver.binaryDirections.length);
-//
-//		double maxPrediction = 0.0;
-//		int indexToAdd = -1;
-//
-//		for (int i = 0; i < EnvironmentObserver.binaryDirections.length; i++) {
-//			possibleActionSets.add(getSameActions(
-//					EnvironmentObserver.binaryDirections[i], matchingSet));
-//			double actualPrediction = 0.0;
-//			for (int j = 0; j < possibleActionSets.get(i).size(); j++) {
-//				actualPrediction = actualPrediction
-//						+ possibleActionSets.get(i).get(j).getPrediction();
-//			}
-//			if (possibleActionSets.get(i).size() != 0) {
-//				actualPrediction = actualPrediction
-//						/ possibleActionSets.get(i).size();
-//			}
-//			if (actualPrediction > maxPrediction) {
-//				maxPrediction = actualPrediction;
-//				indexToAdd = i;
-//			}
-//
-//		}
-
-		double maxPrediction = 0.1;
+		double averagePrediction = getAveragePrediction(matchingSet);
+		double maxPrediction = Math.abs(averagePrediction); // Möchte gerne
+															// positiven Reward
+															// haben.
 		int indexToAdd = -1;
 		for (int i = 0; i < matchingSet.size(); i++) {
-			double elementPrediction = matchingSet.get(i).getPrediction() * matchingSet.get(i).getSpecifity();
+			double elementPrediction = matchingSet.get(i).getPrediction()
+					* matchingSet.get(i).getSpecifity();
 			if (elementPrediction >= maxPrediction) {
 				maxPrediction = elementPrediction;
 				indexToAdd = i;
 			}
 		}
-		
-//		System.out.println("ACHTUNG: " + indexToAdd);
-//		System.out.println("Prediction: "+ maxPrediction);
-		
-//		if (maxPrediction <= 0.001) {
-//
-//			indexToAdd = -1;
-//
-//		}
-		
-		
-		
-		if(indexToAdd != -1){
-			actionSet.addAll(getSameActions(matchingSet.get(indexToAdd).getAction(), matchingSet));
+
+		if (indexToAdd != -1) {
+			actionSet.addAll(getSameActions(matchingSet.get(indexToAdd)
+					.getAction(), matchingSet));
 			matchingSetMinusActionSet.addAll(matchingSet);
 			matchingSetMinusActionSet.removeAll(actionSet);
-				
-		}else{
-			matchingSetMinusActionSet.addAll(matchingSet);
-		}
-		
-		
-
-
-
-//		for (int i = 0; i < EnvironmentObserver.binaryDirections.length; i++) {
-//			if (indexToAdd == i) {
-//				actionSet.addAll(possibleActionSets.get(i));
-//			} else {
-//				matchingSetMinusActionSet.addAll(possibleActionSets.get(i));
-//			}
-//		}
-
-		// for (int i = 0; i < matchingSet.size(); i++) {
-		// if (i == indexToAdd) {
-		// actionSet.add(matchingSet.get(i));
-		// } else {
-		// matchingSetMinusActionSet.add(matchingSet.get(i));
-		// }
-		// }
-		if(indexToAdd != -1)
 			return matchingSet.get(indexToAdd);
-		else
+
+		} else {
+			matchingSetMinusActionSet.addAll(matchingSet);
 			return null;
+		}
+
 	}
 
 	private ArrayList<IStarCSObject> getSameActions(String action,
 			ArrayList<IStarCSObject> matchingSet) {
 		ArrayList<IStarCSObject> sameActions = new ArrayList<IStarCSObject>();
-		System.out.println("Action: "+ action);
 
 		for (int i = matchingSet.size() - 1; i >= 0; i--) {
 			if (matchingSet.get(i).getAction().equals(action)) {
@@ -104,6 +51,19 @@ public class ActionChooser implements IActionChooser {
 		}
 
 		return sameActions;
+	}
+
+	private double getAveragePrediction(ArrayList<IStarCSObject> classifierList) {
+		double predictionSum = 0;
+		for (int i = 0; i < classifierList.size(); i++) {
+			predictionSum = predictionSum
+					+ classifierList.get(i).getPrediction();
+		}
+
+		if (classifierList.size() > 0) {
+			return predictionSum / classifierList.size();
+		} else
+			return 0.0;
 	}
 
 	@Override
